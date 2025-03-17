752c5c0c9fc8fc59c5fa1db2c25a94a56f6126a2
codystack <ebuzzadvert@gmail.com>
2025-03-17 09:50:08 +0100

    First index commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE HTML>
+<html lang="en-US" class="landing-fullscreen"><head>
+
+    <meta charset="utf-8">
+    <title>Él Haven :: Make-over | Make-up | Salon | Hair Extension</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="Hi boo!!! I'm Ebi, the head MUA at EL Haven. With over 6 years experience in the world of makeup, I have perfected the art of creating a timeless airbrushed radiant look. My specialty lies in giving you a luminous glow radiating from your skin that enhances your natural beauty and leaves you looking like a stunning and luxurious version of yourself. Get ready to indulge in the ultimate beauty experience at El Haven."/>
+    <meta name="keywords" content="Make-up, Wig Making, Soft Glam, Gele in Uk, Gele, Bridal Make-up, Civil Wedding, White Wedding, Traditional Wedding, Frontal Wig Installation, Wig Revamping, Wig Washing, Wig Treatment, Wig Styling, Closure, Cornrow"/>
+    <meta name="author" content="webify.com.ng"/>
+
+    <link rel="icon" href="images/el_favicon.png" />
+
+    <link type="text/css" rel="stylesheet" href="css/reset.css">
+    <link type="text/css" rel="stylesheet" href="includes/fontawesome/icons.css">
+    <link type="text/css" rel="stylesheet" href="js/custom-scrollbar/custom-scrollbar.css">
+    <link type="text/css" rel="stylesheet" href="js/magnific-popup/magnific-popup.css">
+    <link type="text/css" rel="stylesheet" href="js/flexslider/flexslider.css">
+    <link type="text/css" rel="stylesheet" href="js/kenburns/kenburns.css">
+    <link type="text/css" rel="stylesheet" href="js/swiper/swiper.css">
+    <link type="text/css" rel="stylesheet" href="css/styles.css">
+    <link type="text/css" rel="stylesheet" href="css/dark.css">
+
+</head>
+
+<body class="landing-fullscreen">
+
+    <div class="preloader">
+        <p>LOADING</p>
+        <span class="circle"></span>
+    </div>
+    
+    <div class="landing-page landing-strips landing-strips-pages">
+
+        <div class="logo-container">
+            <a href="./">
+                <img src="images/el-haven_logo.svg" alt="">
+            </a>
+        </div>
+
+        <div class="row strips two-columns">
+
+            <div class="column" style="background-image:url('images/salon.jpg');">
+                <a href="https://salon.elhaven.co.uk" target="_blank">Salon</a>
+            </div>
+
+            <div class="column col-sm-12" style="background-image:url('images/shop.jpg');">
+                <a href="https://shop.elhaven.co.uk" target="_blank">Shop</a>
+            </div>
+
+        </div>
+
+    </div>
+    
+    <script src="js/jquery.js"></script>
+    <script src="js/modernizr.js"></script>
+    <script src="js/easing.js"></script>
+    <script src="js/imagesloaded.js"></script>
+    <script src="js/fitvids.js"></script>
+    <script src="js/yt-player.js"></script>
+    <script src="js/appear.js"></script>
+    <script src="js/magnific-popup/magnific-popup.js"></script>
+    <script src="js/custom-scrollbar/custom-scrollbar.js"></script>
+    <script src="js/flexslider/flexslider.js"></script>
+    <script src="js/kenburns/kenburns.js"></script>
+    <script src="js/swiper/swiper.js"></script>
+    <script src="js/isotope.js"></script>
+    <script src="js/scripts.js"></script>
+
+
+</body>
+
+</html>
